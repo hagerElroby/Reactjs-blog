621c0d599f36e89150b9976d62b21d2ed34eb94d
hagerElroby <hagerelroby.az2020@gmail.com>
2023-12-23 09:59:36 +0200

    uploading

---
+++ src/App.js
@@ -0,0 +1,123 @@
+import Header from './Header';
+import Nav from './Nav';
+import Footer from './Footer';
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+import Home from './Home';
+import About from './About';
+import Missing from './Missing';
+import {useState , useEffect} from 'react';
+// import {Route , Switch , useNavigate} from 'react-router-dom'
+import { Routes, Route , useNavigate } from 'react-router-dom';
+import {format} from 'date-fns';
+import api from './api/posts'
+import EditPost from './EditPost';
+import useWindowSize from './hooks/useWindowSize';
+
+
+function App() {
+  const navigate = useNavigate();
+  const [posts, setPosts] = useState([])
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [postTitle, setPostTitle] = useState('');
+  const [postBody, setPostBody] = useState('');
+  const [editTitle, setEditTitle] = useState('');
+  const [editBody, setEditBody] = useState('');
+  const {width} = useWindowSize()
+  
+  useEffect(()=>{
+    const fetchPosts = async () => {
+      try{
+        const response = await api.get('/posts');
+        setPosts(response.data);
+      }catch (err){
+        if(err.response){
+          console.log(err.response.data)
+          console.log(err.response.status)
+          console.log(err.response.headers)
+        }else{
+          console.log(`Error: ${err.message}`);
+        }
+      }
+    }
+    fetchPosts();
+  }, [])
+
+  useEffect(()=>{
+    const filteredResults = posts.filter(post => 
+      ((post.body).toLowerCase()).includes(search.toLowerCase())
+      || ((post.title).toLowerCase()).includes(search.toLowerCase()))
+
+      setSearchResults(filteredResults.reverse())
+      
+  } ,[posts , search])
+  
+  const handelDelete = async (id) => {
+    try{
+      await api.delete(`/posts/${id}`)
+      const postsList = posts.filter(post => post.id !== id);
+      setPosts(postsList);
+      navigate('/')
+    }catch(err){
+      console.log(`Error: ${err.message}`);
+    }
+  }
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date() ,'MMMM dd, yyyy pp');
+    const newPost={id , title:postTitle , datetime , body: postBody};
+    try{
+      const response = await api.post('/posts', newPost)
+      const allPosts = [...posts , response.data];
+      setPosts(allPosts);
+      setPostTitle('');
+      setPostBody('');
+      navigate('/')
+    }catch(err){
+      console.log(`Error: ${err.message}`);
+    }
+  }
+
+const handleEdit = async (id) =>{
+  const datetime = format(new Date() ,'MMMM dd, yyyy pp');
+    const updatedPost={id , title:editTitle , datetime , body: editBody};
+
+    try{
+      const response = await api.put(`/posts/${id}`, updatedPost);
+      setPosts(posts.map((post)=>post.id === id ? {...response.data} : post));
+      setEditBody('');
+      setEditTitle('');
+      navigate('/')
+    }catch(err){
+      console.log(`Error: ${err.message}`);
+    }
+}
+
+  return (
+    <div className="App">
+      <Header title="React Js Blog" width={width}/>
+      <Nav search={search} setSearch={setSearch}/>
+      <Routes>
+          <Route path='/' element={<Home posts={searchResults}/>} />
+          <Route path='/post' element={<NewPost
+          handleSubmit={handleSubmit} postBody={postBody}
+          setPostBody={setPostBody} postTitle={postTitle} setPostTitle={setPostTitle}
+          />} />
+          <Route path='/edit/:id' element={<EditPost
+          posts={posts}
+          handleEdit={handleEdit} editBody={editBody}
+          setEditBody={setEditBody} editTitle={editTitle} setEditTitle={setEditTitle}
+          />} />
+          <Route path='/post/:id' element={<PostPage posts={posts} handelDelete={handelDelete}/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='*' element={<Missing/>} />
+      </Routes>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
